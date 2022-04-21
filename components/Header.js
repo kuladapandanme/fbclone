0c668d0ce92b6b6fdc8615f0d941eed2a4fa3e35
bitnami <bitnami@debian>
2022-04-21 07:19:22 +0000

    test

--- components/Header.js
+++ components/Header.js
@@ -4,16 +4,16 @@ import HeaderIcon from './HeaderIcon'
 
 function Header() {
     return (
-        <div className='flex sticky top-0 z-50 bg-white items-center p-2 lg:px-5 shadow-md'>
+        <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md'>
             <div className='flex items-center'>
                 <img src='https://res.cloudinary.com/bellaeenar/image/upload/w_40,h_40,c_fill/fbclone/fb-circle-512.webp' alt='fbclone' width={40} height={40} />
                 <div className='flex ml-2 items-center rounded-full bg-gray-100 p-2'>
                     <SearchIcon className='h-6 text-gray-600' />
-                    <input className='hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500' type='text' placeholder='Search Facebook' name='' id='' />
+                    <input className='hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink' type='text' placeholder='Search Facebook' name='' id='' />
                 </div>
             </div>
-            <div className='flex justify-center flex-grow'>
-                <div className='flex'>
+            <div className='flex justify-center flex-grow md:mx-12'>
+                <div className='flex space-x-6 md:space-x-2'>
                     <HeaderIcon Icon={HomeIcon} active />
                     <HeaderIcon Icon={FlagIcon} />
                     <HeaderIcon Icon={PlayIcon} />
@@ -23,7 +23,7 @@ function Header() {
             </div>
             <div>
                 {/* Profile Pic */}
-                <p className='whitespace-nowrap font-semibold pr-3'>Bella Eenar</p>
+                <p className='whitespace-nowrap font-semibold pr-3'>Kulada Pandan</p>
             </div>
         </div>
 
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,6 +1,6 @@
 function HeaderIcon({ Icon, active }) {
   return ( 
-    <div className="group flex items-center cursor-pointer h-14 px-6 sm:px-10 md:hover:bg-gray-100 rounded-xl md:active:border-b-2 md:active:border-blue-500">
+    <div className="group flex items-center cursor-pointer sm:h-14 md:px-10 md:hover:bg-gray-100 rounded-xl md:active:border-b-2 md:active:border-blue-500">
       <Icon className={`h-5 md:h-7 text-center mx-auto text-gray-500 group-hover:text-blue-500 ${active && 'text-blue-500'} `} />
     </div>
   )
