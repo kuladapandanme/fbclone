a9e3e563ead475ab65b147fcff2b5b5b63e5c54f
bitnami <bitnami@debian>
2022-04-21 07:19:22 +0000

    test

---
+++ _next/static/MghmpCrLG7WU8awcBmANc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f11de2a97f17fac5.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
