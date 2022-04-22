86face950c9613e6c01dcca44fffc260644e9492
bitnami <bitnami@debian>
2022-04-22 02:33:52 +0000

    temp

--- components/Header.js
+++ components/Header.js
@@ -21,9 +21,13 @@ function Header() {
                     <HeaderIcon Icon={UserGroupIcon} />
                 </div>
             </div>
-            <div>
+            <div className='flex items-center sm:space-x-2 justify-end'>
                 {/* Profile Pic */}
                 <p className='whitespace-nowrap font-semibold pr-3'>Kulada Pandan</p>
+                <ViewGridIcon className='icon' />
+                <ChatIcon className='icon' />
+                <BellIcon className='icon' />
+                <ChevronDownIcon className='icon' />
             </div>
         </div>
 
---
+++ pages/DataDeletionInstructions.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DataDeletionInstructions() {
+  return (
+    <div>DataDeletionInstructions</div>
+  )
+}
+
+export default DataDeletionInstructions
